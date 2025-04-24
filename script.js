7276e735f1384d4a69435cef7936843538fbd9a0
Moinpochi <pochimoin@gmail.com>
2025-04-24 18:48:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 particlesJS("particles-js", {
-"particles": {
-"number": {
-"value": 80,
-"density": {
-"enable": true,
-"value_area": 800
-}
-},
-"color": { "value": "#00f7ff" },
-"shape": {
-"type": "circle",
-"stroke": { "width": 0, "color": "#000000" }
-},
-"opacity": {
-"value": 0.5,
-"random": false
-},
-"size": {
-"value": 3,
-"random": true
-},
-"line_linked": {
-"enable": true,
-"distance": 150,
-"color": "#00f7ff",
-"opacity": 0.4,
-"width": 1
-},
-"move": {
-"enable": true,
-"speed": 3,
-"direction": "none",
-"random": false,
-"out_mode": "out"
-}
-},
-"interactivity": {
-"events": {
-"onhover": { "enable": true, "mode": "repulse" }
-},
-"modes": {
-"repulse": { "distance": 100 }
-}
-},
-"retina_detect": true
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": { "value": "#00f7ff" },
+    "shape": {
+      "type": "circle",
+      "stroke": { "width": 0, "color": "#000000" }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false
+    },
+    "size": {
+      "value": 3,
+      "random": true
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#00f7ff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "none",
+      "random": false,
+      "out_mode": "out"
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": { "enable": true, "mode": "repulse" }
+    },
+    "modes": {
+      "repulse": { "distance": 100 }
+    }
+  },
+  "retina_detect": true
 });
